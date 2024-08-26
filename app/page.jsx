8876acd4e2e1429333e5ff60b1915b14ce24c68e
Tyron Meulens <meulens@mbp-van-tyron.home>
2024-08-26 22:24:01 +0200

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+export default function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className={styles.title}>NextJs 14 Starter kit</h1>
+      </div>
+    </section>
+  )
+}
